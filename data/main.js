a66093ea978abcadd794160de1f9822009df9e6c
Hugo <hugo.lembrez@gmail.com>
2023-09-13 16:27:32 +0200

    agrandissement des pages

--- data/main.js
+++ data/main.js
@@ -4,3 +4,10 @@ function openAdd() {
 function closeAdd() {
   document.getElementById("popupAdd").style.display="none";
 }
+
+function openMin() {
+  document.getElementById("popupMin").style.display="block";
+}
+function closeMin() {
+  document.getElementById("popupMin").style.display="none";
+}
